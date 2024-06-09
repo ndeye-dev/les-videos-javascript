93209bb780275deb711f382f8b1e7943e7a72aed
ndeye ngone gningue <gningngone50@gmail.com>
2024-06-09 02:11:46 +0200

    modifier

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
-/* pour afficher une popope
-alert("Hi !");
-*/
-document.write("Hello world !")
 
-console.log("Problème")
+/*pour declarer une variable
+aussi pour afficher le contenu de la varible
+var age = 14;
+document.write(age);
+
+age = 28
+document.write(age);
+*/
+
+/*operateur particulier typeof
+var age = 15 ;
+
+document.write(typeof age) 
+*/
+
+/*
+var majeur = true;
+
+document.write(typeof majeur);
+
+*/
+/*la saisie d'un utilisateur
+var name = prompt("comment t'appelles-tu ?")
+document.write(name)
+*/
+
+/*document.write()
+alert() / confirm()
+prompt()
+pareInt()
+parseFloat()
+*/
+var age = prompt("Quel age as-tu ?");
+
+age = parseInt(age)
+
+document.write(typeof age);
+
+/*pour les constantes  const : valeur constante (non modifiable pour nombres , chaines , booléans)
+ 
+*/
+
+const PI = 3.14;
+PI = 2;
+
+document.write(PI)
+//pour modifier//
+const data =  14;
+data = "14";
 
 
