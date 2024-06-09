61fa29422e9ffa64e5b283f8986af2b9c5717852
ndeye ngone gningue <gningngone50@gmail.com>
2024-06-09 23:41:51 +0200

    modifier

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+document.write("hello ");
+document.write("word !")
 
-// type de boucle
+document.write("hello ");
+document.write("word !")
 
+document.write("hello ");
+document.write("word !")
 
-let i = 0;
- while(i < 15)
-    [
-        document.write("AAA")
-        i++
-    
+//fonctions
+
+let data = "OK"
+
+function functionName()[
+
+    let  = 14 ;
+    document.write(data2):
+    document.write("test : " + data)
 ]
+functionName();
+
+console.log(data2);
+
+function sum(nb1 , nb2)[
+    return nb1 + nb2;
+]
+ 
+
+
+// les arguments , les instructions ....
