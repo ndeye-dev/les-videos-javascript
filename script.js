80cff8438cf207ec5a90198dfedd60d832cdcbc5
ndeye ngone gningue <gningngone50@gmail.com>
2024-06-09 03:00:42 +0200

    modifier

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
 
-/*pour declarer une variable
-aussi pour afficher le contenu de la varible
-var age = 14;
-document.write(age);
+/*les symboles arythmetiques
 
-age = 28
-document.write(age);
-*/
-
-/*operateur particulier typeof
-var age = 15 ;
-
-document.write(typeof age) 
-*/
-
-/*
-var majeur = true;
-
-document.write(typeof majeur);
-
-*/
-/*la saisie d'un utilisateur
-var name = prompt("comment t'appelles-tu ?")
-document.write(name)
-*/
-
-/*document.write()
-alert() / confirm()
-prompt()
-pareInt()
-parseFloat()
-*/
-var age = prompt("Quel age as-tu ?");
-
-age = parseInt(age)
-
-document.write(typeof age);
-
-/*pour les constantes  const : valeur constante (non modifiable pour nombres , chaines , booléans)
+= + * % / 
+-=, +=, *=, /= , %=
+++, --
  
-*/
+modulo pour recuperer le reste ex 5 / 2 (modulo - % = 1) */
 
-const PI = 3.14;
-PI = 2;
-
-document.write(PI)
-//pour modifier//
-const data =  14;
-data = "14";
+/* type primitif pour les chaines de caractères 
+int, float, str */
 
 
+let N = 20;
+
+N = N + 1 ; // a = a + x
+
+document.write(N);
+
+console. log(N)
